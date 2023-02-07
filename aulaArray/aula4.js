d65fa2fade0ac0af782b84be7274946a9e0ec5dc
Marcus Gajozo <marcusgajozo@gmail.com>
2023-02-07 16:37:36 -0400

    aula4 completa

--- aulaArray/aula4.js
+++ aulaArray/aula4.js
@@ -1,4 +1,4 @@
-// Filter, map, reduce
+// Filter -> Sempre retorna um array, com a mesma quantidade de elementos
 
 // Retorne os números maiores que 10
 //               0  1   2   3  4  5  6  7  8  9   10  11  12
@@ -19,8 +19,27 @@ for (let num of numeros) {
 
 const numerosFiltrados = numeros.filter((valor, indice, array) => {
   //console.log(valor);
-  console.log(indice);
+  // console.log(indice);
   return valor > 10;
 });
 
-console.log(numerosFiltrados);
+const pessoas = [
+  { nome: "Marcus", idade: 62 },
+  { nome: "Ana", idade: 15 },
+  { nome: "Gabriel", idade: 20 },
+  { nome: "Lucas", idade: 35 },
+  { nome: "Rosana", idade: 3 },
+  { nome: "Hevila", idade: 8 },
+];
+
+const pessoasComNomeGrande = pessoas.filter((obj) => obj.nome.length >= 5);
+
+const pessoasComMaisDeCinquentaAnos = pessoas.filter((obj) => obj.idade > 50);
+
+const nomeTerminaComA = pessoas.filter((obj) =>
+  obj.nome.toLowerCase().endsWith("a")
+);
+
+console.log(pessoasComNomeGrande);
+console.log(pessoasComMaisDeCinquentaAnos);
+console.log(nomeTerminaComA);
