e99292f801bcbf20828bfc7f74ded2fa72c6aeb1
Marcus Vinicius Borges Gajozo <marcusg@SES.MS>
2023-04-14 13:03:34 -0400

    aula 7 de javascript - node

---
+++ aulaNode/aula7/server.js
@@ -0,0 +1,29 @@
+// Express é micro framework para web
+const express = require('express');
+const app = express();
+
+// CRUD -> CREATE, READ, UPDATE, DELETE
+//         POST    GET   PUT    DELETE
+
+
+app.get('/', (req, res) => {
+    res.send(`
+    <form action="/" method="POST">
+        Nome do cliente: <input type="text" name="nome">
+        <button type="submit">Olá mundo</button>
+    </form>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Recebi o formulário.')
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entrar em contato com a gente!');
+});
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
